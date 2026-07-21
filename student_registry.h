72bc9eb82921c880969ff8b88ace838acb1abbfa
jecprga30 <jecprga30@scs.local>
2026-07-21 12:47:58 +0800

    larkinsanchez

--- student_registry.h
+++ student_registry.h
@@ -1,12 +1,8 @@
+#ifndef STUDENT_REGISTRY_H
+#define STUDENT_REGISTRY_H
 
-
-#ifndef STUDENT_REGISTRY_H   
-#define STUDENT_REGISTRY_H  
-
-
-#define MAX_STUDENTS 100          
-#define DATA_FILE    "students.dat"  
-
+#define MAX_STUDENTS 100
+#define DATA_FILE    "students.dat"
 
 typedef struct {
     int   id;
@@ -16,7 +12,6 @@ typedef struct {
     int   credits;
 } Student;
 
-
 void addStudent(Student students[], int *count);
 void deleteStudent(Student students[], int *count);
 void displayAllStudents(const Student students[], int count);
@@ -24,8 +19,10 @@ void searchByID(const Student students[], int count);
 void findByGPA(const Student students[], int count);
 void findByMajor(const Student students[], int count);
 
-
 void saveStudentsToFile(const Student students[], int count);
-int  loadStudentsFromFile(Student students[]);   
+int  loadStudentsFromFile(Student students[]);
 
-#endif 
+
+void clearInputBuffer(void);
+
+#endif
